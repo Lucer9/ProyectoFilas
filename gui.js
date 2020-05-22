f6fe637a44bed016fa76e9b62b495034c3d0f4f7
Lucer9 <carlos.aguirreorozco@gmail.com>
2020-05-22 18:30:01 -0500

    FinalGUI

--- gui.js
+++ gui.js
@@ -21,20 +21,23 @@ function selectTab(tab) {
 
 function queue(type) {
   let htmlString = "<table> <th colspan='2'> Resultados </th>";
+  let form = document.getElementById(type + "Form");
+  let m;
+  let general;
+
   switch (type) {
     case "mm1":
-      let form = document.getElementById(type + "Form");
       a = parseFloat(form.elements.namedItem("a").value);
       u = parseFloat(form.elements.namedItem("u").value);
 
       ws = form.elements.namedItem("ws").value;
       wc = form.elements.namedItem("wc").value;
-      
+
       pn = form.elements.namedItem("pn").value;
       pnc = form.elements.namedItem("pnc").value;
 
-      const m = new mm1(a, u);
-      let general = m.general();
+      m = new mm1(a, u);
+      general = m.general();
       htmlString += `
       <tr> <td>ρ</td><td>${general.ro.toFixed(5)}</td></tr>
       <tr> <td>p0</td><td>${general.p0.toFixed(5)}</td></tr>
@@ -50,14 +53,137 @@ function queue(type) {
         htmlString += `<tr><td>Cost</td><td>${totalCost.toFixed(5)}</td></tr>`;
       }
 
-      if(pn!=""){
-        htmlString += `<tr><td>p(${pn})</td><td>${m.pn(pn).toFixed(5)}</td></tr>`;
+      if (pn != "") {
+        htmlString += `<tr><td>p(${pn})</td><td>${m
+          .pn(pn)
+          .toFixed(5)}</td></tr>`;
       }
-      if(pnc!=""){
-        htmlString += `<tr><td>p acc(${pnc})</td><td>${m.pnCumulative(pnc).toFixed(5)}</td></tr>`;
+      if (pnc != "") {
+        htmlString += `<tr><td>p acc(${pnc})</td><td>${m
+          .pnCumulative(pnc)
+          .toFixed(5)}</td></tr>`;
       }
       break;
 
+    case "mms":
+      a = parseFloat(form.elements.namedItem("a").value);
+      u = parseFloat(form.elements.namedItem("u").value);
+      s = parseFloat(form.elements.namedItem("s").value);
+
+      ws = form.elements.namedItem("ws").value;
+      wc = form.elements.namedItem("wc").value;
+
+      pn = form.elements.namedItem("pn").value;
+      pnc = form.elements.namedItem("pnc").value;
+
+      m = new mms(a, u, s);
+      general = m.general();
+      htmlString += `
+          <tr> <td>ρ</td><td>${general.ro.toFixed(5)}</td></tr>
+          <tr> <td>p0</td><td>${general.p0.toFixed(5)}</td></tr>
+          <tr> <td>lq</td><td>${general.lq.toFixed(5)}</td></tr>
+          <tr> <td>l</td><td>${general.l.toFixed(5)}</td></tr>
+          <tr> <td>wq</td><td>${general.wq.toFixed(5)}</td></tr>
+          <tr> <td>w</td><td>${general.w.toFixed(5)}</td></tr>
+          `;
+      if (ws == "" ? wc != "" : wc == "") {
+        alert("Completa ambos campos de servicio");
+      } else if (ws != "" && wc != "") {
+        const totalCost = m.totalCost(parseFloat(wc), parseFloat(ws));
+        htmlString += `<tr><td>Cost</td><td>${totalCost.toFixed(5)}</td></tr>`;
+      }
+
+      if (pn != "") {
+        htmlString += `<tr><td>p(${pn})</td><td>${m
+          .pn(pn)
+          .toFixed(5)}</td></tr>`;
+      }
+      if (pnc != "") {
+        htmlString += `<tr><td>p acc(${pnc})</td><td>${m
+          .pnCumulative(pnc)
+          .toFixed(5)}</td></tr>`;
+      }
+      break;
+    case "mmsk":
+      a = parseFloat(form.elements.namedItem("a").value);
+      u = parseFloat(form.elements.namedItem("u").value);
+      s = parseFloat(form.elements.namedItem("s").value);
+      k = parseFloat(form.elements.namedItem("k").value);
+
+      ws = form.elements.namedItem("ws").value;
+      wc = form.elements.namedItem("wc").value;
+
+      pn = form.elements.namedItem("pn").value;
+      pnc = form.elements.namedItem("pnc").value;
+
+      m = new mmsk(a, u, s, k);
+      general = m.general();
+      htmlString += `
+          <tr> <td>ρ</td><td>${general.ro.toFixed(5)}</td></tr>
+          <tr> <td>p0</td><td>${general.p0.toFixed(5)}</td></tr>
+          <tr> <td>lq</td><td>${general.lq.toFixed(5)}</td></tr>
+          <tr> <td>l</td><td>${general.l.toFixed(5)}</td></tr>
+          <tr> <td>wq</td><td>${general.wq.toFixed(5)}</td></tr>
+          <tr> <td>w</td><td>${general.w.toFixed(5)}</td></tr>
+          `;
+      if (ws == "" ? wc != "" : wc == "") {
+        alert("Completa ambos campos de servicio");
+      } else if (ws != "" && wc != "") {
+        const totalCost = m.totalCost(parseFloat(wc), parseFloat(ws));
+        htmlString += `<tr><td>Cost</td><td>${totalCost.toFixed(5)}</td></tr>`;
+      }
+
+      if (pn != "") {
+        htmlString += `<tr><td>p(${pn})</td><td>${m
+          .pn(pn)
+          .toFixed(5)}</td></tr>`;
+      }
+      if (pnc != "") {
+        htmlString += `<tr><td>p acc(${pnc})</td><td>${m
+          .pnCumulative(pnc)
+          .toFixed(5)}</td></tr>`;
+      }
+      break;
+    case "mg1":
+      a = parseFloat(form.elements.namedItem("a").value);
+      u = parseFloat(form.elements.namedItem("u").value);
+      sd = parseFloat(form.elements.namedItem("sd").value);
+
+
+      ws = form.elements.namedItem("ws").value;
+      wc = form.elements.namedItem("wc").value;
+
+      pn = form.elements.namedItem("pn").value;
+      pnc = form.elements.namedItem("pnc").value;
+
+      m = new mg1(a, u, sd);
+      general = m.general();
+      htmlString += `
+          <tr> <td>ρ</td><td>${general.ro.toFixed(5)}</td></tr>
+          <tr> <td>p0</td><td>${general.p0.toFixed(5)}</td></tr>
+          <tr> <td>lq</td><td>${general.lq.toFixed(5)}</td></tr>
+          <tr> <td>l</td><td>${general.l.toFixed(5)}</td></tr>
+          <tr> <td>wq</td><td>${general.wq.toFixed(5)}</td></tr>
+          <tr> <td>w</td><td>${general.w.toFixed(5)}</td></tr>
+          `;
+      if (ws == "" ? wc != "" : wc == "") {
+        alert("Completa ambos campos de servicio");
+      } else if (ws != "" && wc != "") {
+        const totalCost = m.totalCost(parseFloat(wc), parseFloat(ws));
+        htmlString += `<tr><td>Cost</td><td>${totalCost.toFixed(5)}</td></tr>`;
+      }
+
+      if (pn != "") {
+        htmlString += `<tr><td>p(${pn})</td><td>${m
+          .pn(pn)
+          .toFixed(5)}</td></tr>`;
+      }
+      if (pnc != "") {
+        htmlString += `<tr><td>p acc(${pnc})</td><td>${m
+          .pnCumulative(pnc)
+          .toFixed(5)}</td></tr>`;
+      }
+      break;
   }
   htmlString += "</table>";
   let resultsTable = document.getElementById("resultsTable");
